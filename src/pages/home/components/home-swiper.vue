867665aec1d53ff84e4a93ef624a6cb005fa221e
zhx_tom <2609274343@qq.com[credential]>
2018-07-27 09:51:46 +0800

    调整格式化文件、

--- src/pages/home/components/home-swiper.vue
+++ src/pages/home/components/home-swiper.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="swiper">
         <swiper :options="swiperOption">
-    <!-- slides -->
-    <swiper-slide v-for="item in imgUrls" :key="item.id">
-        <img class="swiper-img" :src="item.url"/>
-    </swiper-slide>
-    <!-- Optional controls -->
-    <div class="swiper-pagination"  slot="pagination"></div>
-  </swiper>
+            <!-- slides -->
+            <swiper-slide v-for="item in imgUrls" :key="item.id">
+                <img class="swiper-img" :src="item.url"/>
+            </swiper-slide>
+            <!-- Optional controls -->
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
     </div>    
 </template>
 
@@ -17,32 +17,42 @@ export default {
   data() {
     return {
       swiperOption: {
-          pagination:".swiper-pagination",//增加小圆点
-          loop:true
+        pagination: ".swiper-pagination", //增加小圆点
+        loop: true
       },
-      imgUrls:[
-          {id:"001",url:"https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/dujia_shuqi_banner_20180710.jpg"},
-          {id:"002",url:"https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/anquan_20180716.png"}
-
+      imgUrls: [
+        {
+          id: "001",
+          url:
+            "https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/dujia_shuqi_banner_20180710.jpg"
+        },
+        {
+          id: "002",
+          url:
+            "https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/anquan_20180716.png"
+        }
       ]
-
     };
   }
 };
 </script>
 
 <style lang="stylus" scoped>
-//样式穿透（>>>）父组件里的样式能影响到子组件的样式，在加了scoped后
-.swiper >>> .swiper-pagination-bullet-active
-    background:#fff
-.swiper
-    //保持图片的宽高比
+// 样式穿透（>>>）父组件里的样式能影响到子组件的样式，在加了scoped后
+.swiper >>> .swiper-pagination-bullet-active {
+    background: #fff;
+}
+
+.swiper {
+    // 保持图片的宽高比
     // width:100%
     // overflow:hidden
     // height:0px
     // padding-bottom:31.25%
-    background: #ccc//图片未显示显示灰色背景
-    .swiper-img 
-        width: 100% //让轮播图片正常显示
-    
+    background: #ccc; // 图片未显示显示灰色背景
+
+    .swiper-img {
+        width: 100%; // 让轮播图片正常显示
+    }
+}
 </style>
