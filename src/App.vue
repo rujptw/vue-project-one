fdc6e45951603ace4830e3885609784a0f005454
rujptw <2609274343@qq.com>
2018-08-02 00:07:22 +0800

    keep-alive

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="app">
-    <router-view/>
+    <!-- 
+    kepp-alive会将数据保存到内存中，
+    一次重新渲染会从内存中区取得对应的数据，
+    也就是说他不会调用钩子函数 ，能够提高性能
+    -->
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -15,15 +15,20 @@ import homeIcons from "./components/home-icons";
 import homeRecommend from "./components/home-recommend";
 import homeWeekend from "./components/home-weekend";
 import axios from "axios";
+import { mapState } from "vuex";
 export default {
   name: "Home",
   data() {
     return {
+      lastCity:'',
       swiperList: [],
       iconList: [],
       recommendList: [],
       weekendList: []
     };
+  },
+  computed:{
+    ...mapState(['city'])
   },
   components: {
     //组件：引入的名字要与注册的名字一致
@@ -36,7 +41,7 @@ export default {
   methods: {
     getHomeInfo() {
       axios
-        .get("/api/index.json")
+        .get("/api/index.json?city=" + this.city)
         .then(res => {
           //使用箭头函数绑定当前作用域
           if (res.data.ret && res.data.data) {
@@ -52,7 +57,16 @@ export default {
     }
   },
   mounted() {
+    //lastCity为data里的数据，city为store里的数据
+    this.lastCity =this.city;
     this.getHomeInfo();
+  },
+  //页面重新显示调用此生命周期钩子,切换城市以渲染不同的页面
+  activated(){
+   if(this.lastCity !== this.city){
+     this.lastCity = this.city;
+     this.getHomeInfo();
+   }
   }
 };
 </script>
